4d4367b3e96bcddcfaae0d5bc5e087df0c20daef
jewon shin <jw8421ss@nate.com>
2024-10-16 21:23:54 +0900

    Add new project files

---
+++ src/main/java/com/concert_reservation/hhplusconsertreservation/HhplusConsertReservationApplication.java
@@ -0,0 +1,13 @@
+package com.concert_reservation.hhplusconsertreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusConsertReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusConsertReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concert_reservation/hhplusconsertreservation/HhplusConsertReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concert_reservation.hhplusconsertreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusConsertReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
