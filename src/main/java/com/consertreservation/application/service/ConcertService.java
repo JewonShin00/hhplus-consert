6bad2713c903489ed3d1544bfe7725035d6d6385
Jewon Shin <jw8421ss@nate.com>
2024-10-29 19:35:35 +0900

    Clean repository

--- src/main/java/com/consertreservation/application/service/ConcertService.java
+++
@@ -1,7 +0,0 @@
-package com.consertreservation.application.service;
-
-import java.util.List;
-
-public interface ConcertService {
-    List<Object> getAvailableConcerts(String date); //메서드의 시그니처
-}
